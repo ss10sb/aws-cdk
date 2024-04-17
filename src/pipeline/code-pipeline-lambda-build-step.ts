d8327fbafd172b81d4d779c880245dea2bdc29fc
Scott Morken <scott.morken@phoenixcollege.edu>
2024-04-17 14:34:54 -0700

    "Update package version and comment out PHP artisan cache command"
    This commit updates the version of the '@smorken/aws-cdk' package from 2.130.6 to 2.130.7 in the package.json file. Additionally, the 'php artisan route:cache' command in the code-pipeline-lambda-build-step.ts file has been commented out. The reasons and impacts on the overall project functionality will be analyzed and communicated accordingly.

--- src/pipeline/code-pipeline-lambda-build-step.ts
+++ src/pipeline/code-pipeline-lambda-build-step.ts
@@ -56,7 +56,7 @@ export class CodePipelineLambdaBuildStep extends NonConstruct {
             'cp .env.example .env',
             'composer install --ignore-platform-reqs --no-ansi --no-autoloader --no-dev --no-interaction --no-scripts --no-progress',
             'composer dump-autoload --optimize --classmap-authoritative',
-            'php artisan route:cache',
+            //'php artisan route:cache',
             'rm -rf vendor/bin',
             'rm -f .env',
             'npm ci',
